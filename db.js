88202527d3daad26c9b36fa2c326941de49ad965
Sohail HSD <knight.hsd19@gmail.com>
2022-04-09 15:30:26 +0500

    Connected to atlus.

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoURI = process.env.DATABASE_URI
 const mongoAtlus = `${process.env.MONGO_ATLUS}`
 const uri = 'mongodb://localhost:27017/mybrary'
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
+    mongoose.connect(mongoAtlus, () => {
         console.log('Connected to mongo Successfully...! (db)')
     })
 }
