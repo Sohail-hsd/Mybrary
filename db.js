094c02638162c339ebb335061844988b3dff5e36
sohail HSD <70771040+Sohail-hsd@users.noreply.github.com>
2022-04-09 15:42:47 +0500

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoURI = process.env.DATABASE_URI
 const mongoAtlus = `${process.env.MONGO_ATLUS}`
 const uri = 'mongodb://localhost:27017/mybrary'
-const MONGO_ATLUS = 'mongodb+srv://HSD-19:Terminus919@cluster0.kgnol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGO_ATLUS = 'mongodb+srv://HSD-19:{Terminus919@}cluster0.kgnol.mongodb.net/Express?retryWrites=true&w=majority'
 const connectToMongo = () => {
     mongoose.connect(MONGO_ATLUS, () => {
         console.log('Connected to mongo Successfully...! (db)')
